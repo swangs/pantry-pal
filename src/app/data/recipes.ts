0b3dc898994ea35954f1f20748f21d85d45fd6ea
Steven Wang <sw1190@gmail.com>
2018-02-19 12:03:44 -0800

    'update api'

--- src/app/data/recipes.ts
+++ src/app/data/recipes.ts
@@ -1,40 +1,41 @@
 class Recipe {
   id: number;
-  name: string;
-  imageUrl: string;
-  directions: string;
-
+  title: string;
+  image: string;
+  instructions: string;
 }
 
 export const RECIPES: Recipe[] = [
   {
     id: 1,
-    name: 'fried rice',
-    imageUrl: 'http://www.seriouseats.com/recipes/images/2016/01/20160206-fried-rice-food-lab-68-1500x1125.jpg',
-    directions: 'fry everything together'
+    title: 'fried rice',
+    image: 'http://www.seriouseats.com/recipes/images/2016/01/20160206-fried-rice-food-lab-68-1500x1125.jpg',
+    instructions: 'fry everything together',
+    test: "random",
+    test2: "asdf",
   },
   {
     id: 2,
-    name: 'omelet',
-    imageUrl: 'https://x9wsr1khhgk5pxnq1f1r8kye-wpengine.netdna-ssl.com/wp-content/uploads/basic-french-omelet-930x550.jpg',
-    directions: 'crack an egg, cook it'
+    title: 'omelet',
+    image: 'https://x9wsr1khhgk5pxnq1f1r8kye-wpengine.netdna-ssl.com/wp-content/uploads/basic-french-omelet-930x550.jpg',
+    instructions: 'crack an egg, cook it'
   },
   {
     id: 3,
-    name: 'french fries',
-    imageUrl: 'https://smittenkitchendotcom.files.wordpress.com/2017/03/easier-french-fries.jpg?w=750',
-    directions: 'slice potato, throw it in oil'
+    title: 'french fries',
+    image: 'https://smittenkitchendotcom.files.wordpress.com/2017/03/easier-french-fries.jpg?w=750',
+    instructions: 'slice potato, throw it in oil'
   },
   {
     id: 4,
-    name: 'onion rings',
-    imageUrl: 'https://www.mylatinatable.com/wp-content/uploads/2016/01/foto-heroe-1024x693-1-1024x693.jpg',
-    directions: 'cover onion in batter, throw it in oil'
+    title: 'onion rings',
+    image: 'https://www.mylatinatable.com/wp-content/uploads/2016/01/foto-heroe-1024x693-1-1024x693.jpg',
+    instructions: 'cover onion in batter, throw it in oil'
   },
   {
     id: 5,
-    name: 'applsauce',
-    imageUrl: 'https://www.simplyrecipes.com/wp-content/uploads/2014/10/applesauce-horiz-a2-2000.jpg',
-    directions: 'mash apples'
+    title: 'applsauce',
+    image: 'https://www.simplyrecipes.com/wp-content/uploads/2014/10/applesauce-horiz-a2-2000.jpg',
+    instructions: 'mash apples'
   },
 ];
--- src/app/recipe.service.ts
+++ src/app/recipe.service.ts
@@ -4,6 +4,9 @@ import { RECIPES } from './data/recipes.ts';
 
 import { Observable } from 'rxjs/Observable';
 import { of } from 'rxjs/observable/of';
+var unirest = require('unirest');
+const axios = require('axios');
+
 
 @Injectable()
 export class RecipeService {
@@ -17,5 +20,25 @@ export class RecipeService {
   getRecipe(id: number): Observable<Recipe> {
     return of(RECIPES.find(recipe => recipe.id === id));
   }
+
+  // getRecipe(id: number): Observable<Recipe> {
+  //   return unirest.get("https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/479101/information?includeNutrition=false")
+  //   .header("X-Mashape-Key", "95888ikv6vmshpwMcAndhn7AvAMyp1H51Qnjsn7XTBMDjflTIq")
+  //   .header("Accept", "application/json")
+  //   .end(function (result) {
+  //     console.log(result);
+  //   });
+  // }
+
+  // getRecipe(id: number): Observable<Recipe> {
+  //   return of(axios ({
+  //     method: 'get',
+  //     url: `https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/${id}/information?includeNutrition=false`
+  //     headers: {
+  //       'X-Mashape-Key': '95888ikv6vmshpwMcAndhn7AvAMyp1H51Qnjsn7XTBMDjflTIq',
+  //       'Accept': 'application/json'
+  //      },
+  //   }).then(response => return response.data)
+  // }
 
 }
