d8f16c037d1da944f0664e3f393e252d1fc457c1
Jonathan Abantao <jonabantao@gmail.com>
2018-02-20 01:01:18 -0800

    Remove database from git files

--- config/database.js
+++
@@ -1,9 +0,0 @@
-// const mongoose = require('mongoose');
-
-// mongoose.Promise = global.Promise;
-// mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/PantryPal');
-
-module.exports = {
-  database: process.env.MONGODB_URI || 'mongodb://localhost:27017/PantryPal',
-  secret: 'temporarysecret'
-};
