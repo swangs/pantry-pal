6d8d00f2dd019f5457c142b216155945b475289b
Steven Wang <sw1190@gmail.com>
2018-02-19 17:12:36 -0800

    update service

--- frontend/src/app/recipe.service.ts
+++ frontend/src/app/recipe.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { mashapeKey } from '.api';
+import { mashapeKey } from './api';
 import { Recipe } from './data/recipes';
 import { RECIPES } from './data/recipes';
 import { INGREDIENTS } from './data/ingredients';
@@ -22,27 +22,27 @@ export class RecipeService {
     private http: HttpClient
   ) { }
 
-  getRecipes(): Observable<Recipes[]> {
-    return of(RECIPES);
-  }
-
-  getRecipe(id: number): Observable<Recipe> {
-    return of(RECIPES.find(recipe => recipe.id === id));
-  }
-
   // getRecipes(): Observable<Recipes[]> {
-  //   let ingredients = [];
-  //   for (let i = 0; i < INGREDIENTS.length; i++) {
-  //     ingredients.push(INGREDIENTS[i].name)
-  //   }
-  //   ingredients = ingredients.join(',');
-  //   ingredients = encodeURIComponent(ingredients);
-  //   console.log(ingredients)
-  //   return this.http.get(`https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/findByIngredients?fillIngredients=false&ingredients=${ingredients}&limitLicense=false&number=10&ranking=2`, httpOptions)
+  //   return of(RECIPES);
   // }
   //
   // getRecipe(id: number): Observable<Recipe> {
-  //   return this.http.get(`https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/${id}/information?includeNutrition=false`, httpOptions);
+  //   return of(RECIPES.find(recipe => recipe.id === id));
   // }
+
+  getRecipes(): Observable<Recipes[]> {
+    let ingredients = [];
+    for (let i = 0; i < INGREDIENTS.length; i++) {
+      ingredients.push(INGREDIENTS[i].name)
+    }
+    ingredients = ingredients.join(',');
+    ingredients = encodeURIComponent(ingredients);
+    console.log(ingredients)
+    return this.http.get(`https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/findByIngredients?fillIngredients=false&ingredients=${ingredients}&limitLicense=false&number=10&ranking=2`, httpOptions)
+  }
+
+  getRecipe(id: number): Observable<Recipe> {
+    return this.http.get(`https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/${id}/information?includeNutrition=false`, httpOptions);
+  }
 
 }
